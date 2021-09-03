23b401908150fa38cc36c4a7b28c4cbd5fb97634
Jakub Mróz <j.mroz@Jakubs-Mac-mini-2.local>
2021-09-03 16:12:27 +0200

    Decorator

---
+++ structural_patterns/Decorator.php
@@ -0,0 +1,65 @@
+<?php
+
+interface Window
+{
+    public function renderWindow(): string;
+}
+
+class BasicWindow implements Window
+{
+    public function renderWindow(): string
+    {
+        return 'New window';
+    }
+}
+
+interface WindowDecorator
+{
+    public function __construct(Window $window);
+}
+
+class ScrollbarDecorator implements WindowDecorator, Window
+{
+    private Window $window;
+
+    public function __construct(Window $window)
+    {
+        $this->window = $window;
+    }
+
+    public function renderWindow(): string
+    {
+        return $this->window->renderWindow().$this->renderScrollbar();
+    }
+
+    public function renderScrollbar(): string
+    {
+        return ' with scrollbar';
+    }
+}
+
+class HeaderDecorator implements WindowDecorator
+{
+    private Window $window;
+
+    public function __construct(Window $window)
+    {
+        $this->window = $window;
+    }
+
+    public function renderWindow(): string
+    {
+        return $this->window->renderWindow().$this->renderHeader();
+    }
+
+    public function renderHeader(): string
+    {
+        return ' with header';
+    }
+}
+
+$window = new BasicWindow();
+$window = new ScrollbarDecorator($window);
+$window = new HeaderDecorator($window);
+print $window->renderWindow();
+
