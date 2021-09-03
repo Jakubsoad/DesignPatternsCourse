efdc103e483cf713f96702689ebf4f1be9f07f0a
Jakub Mróz <j.mroz@Jakubs-Mac-mini-2.local>
2021-09-03 15:27:07 +0200

    Composite

--- structural_patterns/Composite.php
+++ structural_patterns/Composite.php
@@ -1,1 +1,45 @@
 <?php
+
+interface FileSystem
+{
+    public function getContent(): string;
+}
+
+class Folder implements FileSystem
+{
+    protected array $files;
+
+    public function getContent(): string
+    {
+        return 'Folder content';
+    }
+
+    public function getFiles(): array
+    {
+        return $this->files;
+    }
+
+    public function addFile(File $file): self
+    {
+        $this->files[] = $file;
+
+        return $this;
+    }
+}
+
+class File implements FileSystem
+{
+    public function getContent(): string
+    {
+        return 'Content of file';
+    }
+}
+
+$file1 = new File();
+$file2 = new File();
+$folder = new Folder();
+
+$folder
+    ->addFile($file1)
+    ->addFile($file2)
+;
