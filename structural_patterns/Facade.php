aad343b83575cfdc7c76bbe2db4ab42a45ff24c1
Jakub Mróz <j.mroz@Jakubs-Mac-mini-2.local>
2021-09-13 15:24:03 +0200

    Facade pattern

---
+++ structural_patterns/Facade.php
@@ -0,0 +1,51 @@
+<?php
+
+class Product
+{
+    public function getProduct(): string
+    {
+        return 'Product';
+    }
+}
+
+class Payment
+{
+    public function makePayment(): bool
+    {
+        return true;
+    }
+}
+
+class Customer
+{
+    public function getCustomerData(): string
+    {
+        return 'Customer Data';
+    }
+}
+
+class OrderFacade
+{
+    private Product $product;
+    private Payment $payment;
+    private Customer $customer;
+
+    public function __construct()
+    {
+        $this->product = new Product();
+        $this->payment = new Payment();
+        $this->customer = new Customer();
+    }
+
+    public function prepareOrder(): string
+    {
+        $this->product->getProduct();
+        $this->payment->makePayment();
+        $this->customer->getCustomerData();
+
+        return 'The order has been completed';
+    }
+}
+
+$order = new OrderFacade();
+print $order->prepareOrder();
