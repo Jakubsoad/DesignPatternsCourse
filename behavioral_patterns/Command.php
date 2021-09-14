c66657c57fb4372d2a3776438424f1c355f7dbb0
Jakub Mróz <j.mroz@Jakubs-Mac-mini-2.local>
2021-09-14 09:38:59 +0200

    Command

---
+++ behavioral_patterns/Command.php
@@ -0,0 +1,65 @@
+<?php
+
+class Engine
+{
+    public function turnOn(): string
+    {
+        return 'Engine is on';
+    }
+
+    public function turnOff(): string
+    {
+        return 'Engine is off';
+    }
+}
+
+interface Command
+{
+    public function execute(): string;
+}
+
+class TurnOnEngineCommand implements Command
+{
+    private Engine $engine;
+
+    public function __construct(Engine $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    public function execute(): string
+    {
+        return $this->engine->turnOn();
+    }
+}
+
+class TurnOffEngineCommand implements Command
+{
+    private Engine $engine;
+
+    public function __construct(Engine $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    public function execute(): string
+    {
+        return $this->engine->turnOff();
+    }
+}
+
+class EngineSwitcher
+{
+    public function useSwitch(Command $command): string
+    {
+        return $command->execute();
+    }
+}
+
+$engine = new Engine();
+$turnOnCommand = new TurnOnEngineCommand($engine);
+$turnOffCommand = new TurnOffEngineCommand($engine);
+$switcher = new EngineSwitcher();
+
+print $switcher->useSwitch($turnOnCommand);
+print $switcher->useSwitch($turnOffCommand);
